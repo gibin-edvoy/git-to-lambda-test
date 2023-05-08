f06ca80e08caca1a2eea017e10a258ab46164926
Gibin <gibin@GMs-MacBook-Pro.local>
2023-05-08 14:53:45 +0530

    added with api

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import json
+import sys
 
 
 def lambda_handler(event, context):
     d = {'col1': [1, 2], 'col2': [3, 4]}
     df = pd.DataFrame(data=d)
+    print("Python version: ", sys.version)
+    print("Version info: ", sys.version_info)
     print(df)
     print('Done with api')
     response = {
